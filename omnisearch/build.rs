8d486aa98cb682ab3a89bc66aa8d394487873956
PranshulSoni <pranshulsoni57@gmail.com>
2026-07-03 23:45:05 +0530

    Configure Windows PE resource metadata (FileDescription, CompanyName, ProductName) in build.rs to help establish reputation and prevent heuristic blocks

--- omnisearch/build.rs
+++ omnisearch/build.rs
@@ -4,6 +4,12 @@ fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default() == "windows" {
         let mut res = winres::WindowsResource::new();
         res.set_icon("../icons/OmniSearchTrans.ico");
+        res.set_language(0x0409); // U.S. English
+        res.set("FileDescription", "OmniSearch Launcher");
+        res.set("ProductName", "OmniSearch");
+        res.set("OriginalFilename", "omnisearch.exe");
+        res.set("CompanyName", "Pranshul Soni");
+        res.set("LegalCopyright", "Copyright (c) 2026 Pranshul Soni");
         res.compile().unwrap();
     }
 }
